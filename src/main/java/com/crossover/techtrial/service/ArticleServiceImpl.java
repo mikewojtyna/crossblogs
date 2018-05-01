388410a14295f3c0bf4c4adba72c1b3833f663ef
Mike Wojtyna <mike@buildmysoftware.pro>
2018-05-02 00:35:59 +0200

    Make article repository title search method more efficient

--- src/main/java/com/crossover/techtrial/service/ArticleServiceImpl.java
+++ src/main/java/com/crossover/techtrial/service/ArticleServiceImpl.java
@@ -36,8 +36,7 @@ public class ArticleServiceImpl implements ArticleService
 	public List<Article> search(String search)
 	{
 		return articleRepository
-			.findTop10ByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(
-				search, search);
+			.findByTitleContainingIgnoreCase(search);
 	}
 
 	/* (non-Javadoc)
